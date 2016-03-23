86ce40ca458ee074e794d6eb96d4c07b789fe14e
Alexandre Chaussier <alexandre.chaussier@temelio.com>
2016-03-23 23:27:29 +0100

    Add hostgroup templating

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -74,5 +74,13 @@ describe 'shinken Ansible role configuration' do
         it { should be_owned_by shinken_user  }
         it { should be_grouped_into shinken_group  }
     end
+
+    # Hostgroup configuration file
+    describe file("#{shinken_path_etc}/hostgroups/linux.cfg") do
+        it { should exist }
+        it { should be_file }
+        it { should be_owned_by shinken_user }
+        it { should be_grouped_into shinken_group }
+    end
 end
 
